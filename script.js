e162923370b30ce9e93186d77b77408600ec982c
marcusryden92 <marcus.ryden1992@gmail.com>
2023-12-01 14:30:36 +0100

    Co-authored-by: Pippa <carmelapi@users.noreply.github.com> Co-authored-by: maryana r. <maryana-reznychenko@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1. create the user (input name)
+// 2. Create a object of icons (max 10) (id 01: 😀, id 02: 😇 )
+// User = icons + name --> 😀 Carmela - 😇 Maryana - 🤪 Marcus
+
+//Show cards from existing divs (pictures)
+// create three cards (back and front)
+// three picture : sun - rain - snow
+
+// Show the cards in front
+// flip cards in back
+// mix thouse cards
+
+// add click to the each card
+// turn (flip) the card
+
+// if it is the right card is The Sun card --> 1 point
