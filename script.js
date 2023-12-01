d6d578ccf51d04cead07eb8fa669122673ac45e3
Pippa <carmela.pippa@hyperisland.se>
2023-12-01 19:20:21 +0100

    Add code for shuffling

--- script.js
+++ script.js
@@ -14,3 +14,25 @@
 // turn (flip) the card
 
 // if it is the right card is The Sun card --> 1 point
+
+/* -------------- Shuffling cards ----------------- */
+
+// let arrayCards = ["sun", "rain", "snow"];
+let $cardSun = document.querySelector("#card0");
+let $cardRain = document.querySelector("#card1");
+let $cardSnow = document.querySelector("#card2");
+
+let arrayCards = [$cardSun, $cardRain, $cardSnow];
+
+arrayCards.sort(
+  () => {
+    if (Math.random() > 0.5) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+  // (Math.random() > 0.5 ? 1 : -1)
+);
+
+console.log(arrayCards);
